3b02045044e80adca60e20bb1d56b4fe8d370159
wangliguang <wangliguang@kaikeba.com>
2022-05-07 13:47:53 +0800

    参数获取

--- src/main/java/com/guang/yuantodo/controller/TodoController.java
+++ src/main/java/com/guang/yuantodo/controller/TodoController.java
@@ -4,11 +4,14 @@ package com.guang.yuantodo.controller;
 import com.guang.yuantodo.entity.Todo;
 import com.guang.yuantodo.enums.TodoTypeEnum;
 import com.guang.yuantodo.mapper.TodoMapper;
+import jdk.nashorn.internal.ir.RuntimeNode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * <p>
@@ -26,11 +29,8 @@ public class TodoController {
     private TodoMapper todoMapper;
 
     @RequestMapping("/create")
-    public String create() {
-        Todo todo = new Todo();
-         todo.setContent("第一个todo");
-         todo.setType(TodoTypeEnum.IM_noUR);
-         todoMapper.insert(todo);
+    public String create(Todo todo, HttpServletResponse response) {
+        todoMapper.insert(todo);
         return todo.toString();
     }
 }
