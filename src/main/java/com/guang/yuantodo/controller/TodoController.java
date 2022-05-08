f6244f0fcb220ac01eb9489b15abbdfc527e67ae
wangliguang <wangliguang@kaikeba.com>
2022-05-09 07:15:03 +0800

    新增查询所有，查询单个todo的接口

--- src/main/java/com/guang/yuantodo/controller/TodoController.java
+++ src/main/java/com/guang/yuantodo/controller/TodoController.java
@@ -3,6 +3,7 @@ package com.guang.yuantodo.controller;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.guang.yuantodo.entity.Todo;
+import com.guang.yuantodo.enums.TodoTypeEnum;
 import com.guang.yuantodo.mapper.TodoMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -44,11 +46,42 @@ public class TodoController {
         return "";
     }
 
+    @GetMapping("")
+    @Transactional
+    public Todo queryById(Integer t_id, HttpServletResponse response) throws Exception {
+        Todo todo = todoMapper.selectById(t_id);
+        if (null == todo) {
+            throw new Exception("该todo不存在");
+        }
+        return todo;
+    }
+
     @GetMapping("/queryAll")
-    public List<Todo> queryAll(HttpServletResponse response) {
-        List<Todo> todoList = todoMapper.selectList(null);
-        HashMap resultMap = new HashMap();
-        return todoList;
+    public HashMap queryAll(HttpServletResponse response) {
+        List<Todo> allTodoList = todoMapper.selectList(null);
+        List<Todo> imUrTodoList = new ArrayList<>();
+        List<Todo> imNoUrTodoList = new ArrayList<>();
+        List<Todo> noImUrTodoList = new ArrayList<>();
+        List<Todo> noImNoUrTodoList = new ArrayList<>();
+        allTodoList.stream().forEach((todo) -> {
+            if (todo.getType() == TodoTypeEnum.IM_UR) {
+                imUrTodoList.add(todo);
+            }
+            if (todo.getType() == TodoTypeEnum.IM_noUR) {
+                imNoUrTodoList.add(todo);
+            }
+            if (todo.getType() == TodoTypeEnum.noIM_UR) {
+                noImUrTodoList.add(todo);
+            }
+            if (todo.getType() == TodoTypeEnum.noIM_noUR) {
+                noImNoUrTodoList.add(todo);
+            }});
+        HashMap data = new HashMap();
+        data.put("imUr", imUrTodoList);
+        data.put("imNoUr", imNoUrTodoList);
+        data.put("noImUr", noImUrTodoList);
+        data.put("noImNoUr", noImNoUrTodoList);
+        return data;
     }
 
 }
